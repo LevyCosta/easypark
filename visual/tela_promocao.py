aa8cca5b4610850d81863987f8ad1d8d235d478a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-07-19 20:35:40 -0300

    Adicionei elementos GUI na tela promocao

--- visual/tela_promocao.py
+++ visual/tela_promocao.py
@@ -10,9 +10,12 @@ class TelaPromocao:
     def init_components(self):
         sg.ChangeLookAndFeel('Reddit')
         layout_promo = [
-            [sg.Text('Promoções ativas:', justification='c', size=(15, 1), font=('Helvetica', 12))]
+            [sg.Text('Promoções ativas:', size=(15, 1), font=('Helvetica', 12))],
+            [sg.Text('Promo Ativa 1 (só exemplo - achar a list correta)')],
+            [sg.Text('Promo Ativa 2')],
+            [sg.Button('Novo'), sg.Button('Remover'), sg.Button('Editar')]
         ]
-        self.__window = sg.Window(title='Promoções', layout=layout_promo, default_button_element_size=(20, 1), element_justification='l', size=(400, 400), resizable=True)
+        self.__window = sg.Window(title='Promoções', layout=layout_promo, default_button_element_size=(20, 1), element_justification='c', size=(400, 400), resizable=True)
 
     def open(self):
         while True:

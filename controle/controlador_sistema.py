27f383b6452783ee6feba8e59e86253dc68c9e73
Levy Costa <levycosta471@gmail.com>
2021-07-19 20:36:22 -0300

    Revert "Merge pull request #1 from barbaracalderon/barbaracalderon"
    This reverts commit 441271fdb89e2af59d41f7d074b70aadfde36f7d.

--- controle/controlador_sistema.py
+++ controle/controlador_sistema.py
@@ -1,7 +1,6 @@
 import PySimpleGUI as sg
 from visual.tela_sistema import TelaSistema
 from controle.controlador_estacionamento import ControladorEstacionamento
-from controle.controlador_promocao import ControladorPromocao
 
 class ControladorSistema():
     __instance = None
@@ -9,7 +8,6 @@ class ControladorSistema():
     def __init__(self):
         self.__tela_sistema = TelaSistema(self)
         self.__controlador_estacionamento = ControladorEstacionamento(self)
-        self.__controlador_promocao = ControladorPromocao(self)
 
     def __new__(cls):
         if ControladorSistema.__instance is None:
@@ -28,13 +26,14 @@ class ControladorSistema():
             if button == sg.WIN_CLOSED:
                 exit(0)
             funcao_escolhida = switcher[button]
+
             funcao_escolhida()
 
     def opcao_vagas(self):
         self.__controlador_estacionamento.abre_tela()
 
     def opcao_promocoes(self):
-        self.__controlador_promocao.abre_tela()
+        pass
 
     def opcao_mensalistas(self):
         pass

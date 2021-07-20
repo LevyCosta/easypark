7f64952ced1a0bdc6b3cd19423b2605636b2b620
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-07-19 22:58:15 -0300

    Nova view criada tela_promo_novo_editar

--- controle/controlador_promocao.py
+++ controle/controlador_promocao.py
@@ -1,4 +1,5 @@
 from visual.tela_promocao import TelaPromocao
+from visual.tela_promocao_novo_editar import TelaPromocaoNovoEditar
 from entidade.promocao import Promocao
 import PySimpleGUI as sg
 
@@ -8,14 +9,44 @@ class ControladorPromocao:
         self.__controlador = controlador_sistema
         self.__promocao = Promocao
         self.__tela_promocao = TelaPromocao(self)
+        self.__tela_promocao_novo_editar = TelaPromocaoNovoEditar(self)
 
     def abre_tela(self):
+        switcher = {'Novo': self.opcao_novo, 'Remover': self.opcao_remover, 'Editar': self.opcao_editar}
+        while True:
+            button, values = self.__tela_promocao.open()
+            if button == sg.WIN_CLOSED:
+                exit(0)
+            else:
+                funcao_escolhida = switcher[button]
+                funcao_escolhida()
+
+    def opcao_novo(self):
+        switcher = {'Salvar': self.salvar_desconto, 'Cancelar': self.retornar}
+        while True:
+            button, values = self.__tela_promocao_novo_editar.open()
+            if button == sg.WIN_CLOSED:
+                exit(0)
+            else:
+                funcao_escolhida = switcher[button]
+                funcao_escolhida()
+
+    def salvar_desconto(self):
+        pass
+
+    def retornar(self):
+        self.__tela_promocao_novo_editar.close()
         self.__tela_promocao.open()
+
+    def opcao_remover(self):
         pass
 
-
-    def salvarDesconto(self):
-        pass
-
-    def retorna(self):
-        pass
+    def opcao_editar(self):
+        switcher = {'Salvar': self.salvar_desconto, 'Cancelar': self.retornar}
+        while True:
+            button, values = self.__tela_promocao_novo_editar.open()
+            if button == sg.WIN_CLOSED:
+                exit(0)
+            else:
+                funcao_escolhida = switcher[button]
+                funcao_escolhida()

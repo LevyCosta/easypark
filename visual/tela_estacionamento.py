d8e0f728f542ea6749ce53c6f4a1ad5259dbb763
Lívia Marques <liviasmqs@gmail.com>
2021-07-19 23:11:08 -0300

    Tela de detalhes do estacionamento atualizada conforme protótipo

--- visual/tela_estacionamento.py
+++ visual/tela_estacionamento.py
@@ -1,7 +1,6 @@
 import PySimpleGUI as sg
 
 
-
 class TelaEstacionamento():
 
     def __init__(self, controlador_estacionamento):
@@ -12,13 +11,26 @@ class TelaEstacionamento():
     def init_components(self):
         sg.ChangeLookAndFeel('Reddit')
         layout = [
-            [sg.Text('EasyPark', justification='c', size=(20, 1), font=("Helvetica", 14))],
-            [sg.Text('Número de vagas para carros:'), sg.Input(key='numCarro', default_text=self.__controlador.getNumeroVagasCarros())],
-            [sg.Text('Número de vagas para moto:'), sg.Input(key='numMoto', default_text=self.__controlador.getNumeroVagasMotos())],
+            [sg.Text('Informações gerais do estacionamento Park', justification='c', font=("Helvetica", 14))],
+            [sg.Text(' ')],
+            [sg.Text('Valores do estacionamento', justification='c', font=("Helvetica", 12))],
+            [sg.Text('Carro (segunda a sexta)'), sg.Text('                  '), sg.Text('Moto (segunda a sexta)')],
+            [sg.Text('Por hora: R$8,00'), sg.Text('                          '),sg.Text('Por hora: R$6,00')],
+            [sg.Text('Diária: R$35,00'), sg.Text('                          '), sg.Text('Diária: R$30,00')],
+            [sg.Text('Pernoite: R$35,00'), sg.Text('                          '), sg.Text('Pernoite: R$30,00')],
+            [sg.Text(' ')],
+            [sg.Text('Carro (sábado e domingo)'), sg.Text('                  '), sg.Text('Moto (sábado e domingo)')],
+            [sg.Text('Por hora: R$4,00'), sg.Text('                          '), sg.Text('Por hora: R$3,00')],
+            [sg.Text('Diária: R$25,00'), sg.Text('                          '), sg.Text('Diária: R$20,00')],
+            [sg.Text('Pernoite: R$35,00'), sg.Text('                          '), sg.Text('Pernoite: R$30,00')],
+            [sg.Text(' ')],
+            [sg.Text('Número de vagas para carros:'), sg.Input(key='numCarro', default_text=self.__controlador.getNumeroVagasCarros(), size=(10,5)),
+             sg.Text('Número de vagas para moto:'), sg.Input(key='numMoto', default_text=self.__controlador.getNumeroVagasMotos(), size=(10,5))],
+            [sg.Text(' ')],
             [sg.Button('Salvar alterações'), sg.Button('Voltar')]
         ]
 
-        self.__window = sg.Window('Estacionamento Park', layout, default_button_element_size=(40, 1), element_justification='c', size=(800, 400), resizable=True)
+        self.__window = sg.Window('Estacionamento Park', layout, default_button_element_size=(40, 1), element_justification='c', size=(800, 420), resizable=True)
 
     def open(self):
         while True:

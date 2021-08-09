eefebe1be7ebf8e34e7d79aef2020a8c2f105b5d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-08-09 20:46:19 -0300

    "stashed changes"

--- visual/tela_promocao.py
+++ visual/tela_promocao.py
@@ -13,8 +13,8 @@ class TelaPromocao:
             [sg.Text('Promoções Ativas', size=(15, 1), font=('Helvetica', 12))],
  #           [sg.Listbox(values=(f'Desconto para Carros de {self.descontoCarro}%',
  #                               f'Desconto para Motos de {descontoMoto}%'), size=(30, 3), key='_LISTBOX_')],
-            [sg.Listbox(values=(f'Desconto para Carros de {descontoCarro}',
-                                f'Desconto para Motos de {descontoMoto}'))],
+            [sg.Listbox(values=(f'Desconto para Carros de {descontoCarro}%',
+                                f'Desconto para Motos de {descontoMoto}%'))],
             [sg.Button('Novo'), sg.Button('Remover'), sg.Button('Editar'), sg.Button('Cancelar')]
         ]
         self.__window = sg.Window(title='Promoções', layout=layout_promo, default_button_element_size=(20, 1), element_justification='c', size=(400, 300), resizable=True)

ee9904d2e20bf461d55889806eafa42a737bfba5
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-08-09 20:48:24 -0300

    "stashed changes"

--- visual/tela_promocao.py
+++ visual/tela_promocao.py
@@ -11,13 +11,12 @@ class TelaPromocao:
         sg.ChangeLookAndFeel('Reddit')
         layout_promo = [
             [sg.Text('Promoções Ativas', size=(15, 1), font=('Helvetica', 12))],
- #           [sg.Listbox(values=(f'Desconto para Carros de {self.descontoCarro}%',
- #                               f'Desconto para Motos de {descontoMoto}%'), size=(30, 3), key='_LISTBOX_')],
             [sg.Listbox(values=(f'Desconto para Carros de {descontoCarro}%',
                                 f'Desconto para Motos de {descontoMoto}%'))],
             [sg.Button('Novo'), sg.Button('Remover'), sg.Button('Editar'), sg.Button('Cancelar')]
         ]
-        self.__window = sg.Window(title='Promoções', layout=layout_promo, default_button_element_size=(20, 1), element_justification='c', size=(400, 300), resizable=True)
+        self.__window = sg.Window(title='Promoções', layout=layout_promo, default_button_element_size=(20, 1),
+                                  element_justification='c', size=(400, 300), resizable=True)
 
     def open(self):
         while True:
@@ -29,5 +28,3 @@ class TelaPromocao:
 
     def mensagem_sucesso(self):
         sg.PopupOK('Promoção ativada com sucesso!', title='Valor da Promoção')
-
-

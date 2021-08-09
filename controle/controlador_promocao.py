3e6a1b611fe99de686cee986d147fd612b615f84
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-08-09 20:36:45 -0300

    Adicionei validação

--- controle/controlador_promocao.py
+++ controle/controlador_promocao.py
@@ -8,12 +8,10 @@ class ControladorPromocao:
 
     def __init__(self, controlador_sistema):
         self.__controlador = controlador_sistema
-        self.__promocaoCarro = Promocao('carro', 0)         #atenção
-        self.__promocaoMoto = Promocao('moto', 0)          #atenção
+        self.__promocaoCarro = Promocao('carro', 0)
+        self.__promocaoMoto = Promocao('moto', 0)
         self.__tela_promocao = TelaPromocao(self)
         self.__tela_promocao_novo_editar = TelaPromocaoNovoEditar(self)
-#        self.__descontoCarro = 0
-#        self.__descontoMoto = 0
 
     def get_desconto_carro(self):
         return self.__promocaoCarro.getDesconto()
@@ -33,7 +31,6 @@ class ControladorPromocao:
                     'Editar': self.opcao_editar,
                     'Cancelar': self.opcao_cancelar}
         while True:
- #           self.__tela_promocao.init_components(self.__descontoCarro, self.__descontoMoto)
             self.__tela_promocao.init_components(self.get_desconto_carro(), self.get_desconto_moto())
             button, values = self.__tela_promocao.open()
             if button == 'Cancelar' or button == sg.WIN_CLOSED:
@@ -46,7 +43,7 @@ class ControladorPromocao:
     def opcao_novo(self):
         self.abre_tela_novo_editar()
 
-    def salvar_desconto(self, desconto=0):
+    def salvar_desconto(self, desconto=0.0):
         self.__tela_promocao_novo_editar.close()
         return desconto
 
@@ -66,36 +63,36 @@ class ControladorPromocao:
         self.set_desconto_moto(0)
         self.set_desconto_carro(0)
 
-#    def opcao_remover(self):
-#        self.__descontoCarro = 0
-#        self.__descontoMoto = 0
-
     def opcao_editar(self):
         self.abre_tela_editar()
 
     def abre_tela_editar(self):
         while True:
-            self.__tela_promocao_novo_editar.init_components()
-            button, values = self.__tela_promocao_novo_editar.open()
-            carro = values['_CARRO_']
-            moto = values['_MOTO_']
-            desconto = values['input_desconto']
-            if desconto == '':
-                desconto = 0
-            else:
-                desconto = int(desconto)
+            while True:
+                self.__tela_promocao_novo_editar.init_components()
+                button, values = self.__tela_promocao_novo_editar.open()
+                carro = values['_CARRO_']
+                moto = values['_MOTO_']
+                desconto = values['input_desconto']
+                desconto_valido = self.fazer_validacao_desconto(desconto)
+                if desconto_valido is not None:
+                    break
+                elif button == 'Cancelar' or button == sg.WIN_CLOSED:
+                    self.__tela_promocao_novo_editar.close()
+                    self.retorna_tela_promo()
+                else:
+                    sg.PopupOK('Valor inválido. Verifique novamente.', title='Erro')
+                self.__tela_promocao_novo_editar.close()
             if button == 'Cancelar' or button == sg.WIN_CLOSED:
                 self.__tela_promocao_novo_editar.close()
                 self.retorna_tela_promo()
             else:
-                valor_desconto = self.salvar_desconto(desconto)
+                valor_desconto = self.salvar_desconto(desconto_valido)
                 veiculo = self.salvar_veiculo(carro, moto)
-#                veiculo_desconto = self.salvar_desconto_tipo(veiculo)
                 if veiculo == 'moto':
                     self.set_desconto_moto(valor_desconto)
                 elif veiculo == 'carro':
                     self.set_desconto_carro(valor_desconto)
-                #self.alocar_atributo(veiculo_desconto, valor_desconto)
                 self.retorna_tela_promo()
 
     def opcao_cancelar(self):
@@ -103,25 +100,28 @@ class ControladorPromocao:
         self.__controlador.abre_tela()
 
     def abre_tela_novo_editar(self):
-        #switcher = {'Salvar': self.salvar_desconto, 'Cancelar': self.retorna_tela_promo}
         while True:
-            self.__tela_promocao_novo_editar.init_components()
-            button, values = self.__tela_promocao_novo_editar.open()
-            carro = values['_CARRO_']
-            moto = values['_MOTO_']
-            desconto = values['input_desconto']
-            if desconto == '':
-                desconto = 0
-            else:
-                desconto = int(desconto)
+            while True:
+                self.__tela_promocao_novo_editar.init_components()
+                button, values = self.__tela_promocao_novo_editar.open()
+                carro = values['_CARRO_']
+                moto = values['_MOTO_']
+                desconto = values['input_desconto']
+                desconto_valido = self.fazer_validacao_desconto(desconto)
+                if desconto_valido is not None:
+                    break
+                elif button == 'Cancelar' or button == sg.WIN_CLOSED:
+                    self.__tela_promocao_novo_editar.close()
+                    self.retorna_tela_promo()
+                else:
+                    sg.PopupOK('Valor inválido. Verifique novamente.', title='Erro')
+                self.__tela_promocao_novo_editar.close()
             if button == 'Cancelar' or button == sg.WIN_CLOSED:
                 self.__tela_promocao_novo_editar.close()
                 self.retorna_tela_promo()
             else:
-                valor_desconto = self.salvar_desconto(desconto)
+                valor_desconto = self.salvar_desconto(desconto_valido)
                 veiculo = self.salvar_veiculo(carro, moto)
-                veiculo_desconto = self.salvar_desconto_tipo(veiculo)
-#                if veiculo == 'carro' and self.__descontoCarro != 0:
                 if veiculo == 'carro':
                     if self.get_desconto_carro() != 0:
                         sg.PopupOK('Já existe um desconto de carro ativo! Não é possível fazer esta operação.',
@@ -136,9 +136,21 @@ class ControladorPromocao:
                         self.retorna_tela_promo()
                     else:
                         self.set_desconto_moto(valor_desconto)
-                #funcao_escolhida = switcher[button]
-                #funcao_escolhida()
                 self.retorna_tela_promo()
+
+    def fazer_validacao_desconto(self, desconto):
+        try:
+            desconto_num = float(desconto)
+            if 0 <= desconto_num <= 100:
+                desconto_valido = desconto_num
+                return desconto_valido
+            else:
+                desconto_invalido = None
+                return desconto_invalido
+        except:
+            desconto_invalido = None
+            return desconto_invalido
+
 
     def alocar_atributo(self, veiculo_desconto, valor_desconto):
         if veiculo_desconto == 'carro':
